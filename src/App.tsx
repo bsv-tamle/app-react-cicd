77fb1d47f51e2885abe7fb06fed143e72d5b12bb
Tam Le <tamle@bravesoft.com.vn>
2024-10-23 10:00:27 +0700

    commit code

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ReactJS with CI/CD AWS
+          ReactJS with CI/CD AWS Deploy
         </a>
       </header>
     </div>
