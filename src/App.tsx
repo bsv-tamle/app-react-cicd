b0b139df233cc97bcc1b1af90146fea2db4c0c2a
Tam Le <tamle@bravesoft.com.vn>
2024-10-23 09:57:33 +0700

    commit code

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn ReactJS with CI/CD
+          ReactJS with CI/CD
         </a>
       </header>
     </div>
